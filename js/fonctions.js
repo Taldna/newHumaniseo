f45279139db57643256d78f0ff33d527ebd6be46
CarreauTh <tho.carreau@gmail.com>
2023-10-09 18:18:55 +0200

    Refactor: separation des différents types de fichiers

---
+++ js/fonctions.js
@@ -0,0 +1,8 @@
+function deroulement() {
+    document.getElementById("close").style.display = "none";
+    document.getElementById("open").style.display = "grid";
+}
+function enroulement() {
+    document.getElementById("open").style.display = "none";
+    document.getElementById("close").style.display = "grid";
+}
